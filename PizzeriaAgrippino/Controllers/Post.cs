e9a11b7397fd4b86139526502d75804ea0cd7b10
Agrippino <agrippinorussiello@outlook.it>
2022-05-10 12:02:09 +0200

    Add project files.

---
+++ PizzeriaAgrippino/Controllers/Post.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PizzeriaAgrippino.Controllers
+{
+    public class Post : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
